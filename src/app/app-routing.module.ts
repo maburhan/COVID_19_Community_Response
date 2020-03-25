36d0dc870b60a928a526036bb508e1d86efd5197
A <burhanuddeenm@ad.harman.com>
2020-03-25 19:00:18 +0530

    Added External Modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HospitalListComponent } from './components/hospital-list/hospital-list.component';
+import { QuarantineCenterListComponent } from './components/quarantine-center-list/quarantine-center-list.component';
+import { IsolationCenterListComponent } from './Components/isolation-center-list/isolation-center-list.component';
+import { TestingCenterListComponent } from './Components/testing-center-list/testing-center-list.component';
+import { AddHospitalComponent } from './components/add-hospital/add-hospital.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'hospitals', component:HospitalListComponent},
+  { path: 'quarantine', component:QuarantineCenterListComponent},
+  { path: 'isolationcenter', component:IsolationCenterListComponent},
+  { path: 'testingcenter', component:TestingCenterListComponent},
+  { path: 'AddHospital', component:AddHospitalComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingComponents = [
+  HospitalListComponent, 
+  QuarantineCenterListComponent,
+  IsolationCenterListComponent,
+  TestingCenterListComponent,
+  AddHospitalComponent
+]
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,219 @@
-import { Component } from '@angular/core';
+import { ElementRef, Component, ViewChild} from '@angular/core';
+import { } from  'googlemaps';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Covid 19 Community';
+
+export class AppComponent   {
+  title = 'Covid 19 Community';  
+  @ViewChild('map', {static: true }) gmapElement : ElementRef;
+  map: google.maps.Map;
+  service: google.maps.places.PlacesService;
+
+  ngOnInit() {
+    
+  // Create a new StyledMapType object, passing it an array of styles,
+        // and the name to be displayed on the map type control.
+        var styledMapType = new google.maps.StyledMapType(
+          [
+            {
+              "elementType": "geometry",
+              "stylers": [
+                {
+                  "color": "#f5f5f5"
+                }
+              ]
+            },
+            {
+              "elementType": "labels.icon",
+              "stylers": [
+                {
+                  "visibility": "off"
+                }
+              ]
+            },
+            {
+              "elementType": "labels.text.fill",
+              "stylers": [
+                {
+                  "color": "#616161"
+                }
+              ]
+            },
+            {
+              "elementType": "labels.text.stroke",
+              "stylers": [
+                {
+                  "color": "#f5f5f5"
+                }
+              ]
+            },
+            {
+              "featureType": "administrative.land_parcel",
+              "elementType": "labels.text.fill",
+              "stylers": [
+                {
+                  "color": "#bdbdbd"
+                }
+              ]
+            },
+            {
+              "featureType": "poi",
+              "elementType": "geometry",
+              "stylers": [
+                {
+                  "color": "#eeeeee"
+                }
+              ]
+            },
+            {
+              "featureType": "poi",
+              "elementType": "labels.text.fill",
+              "stylers": [
+                {
+                  "color": "#757575"
+                }
+              ]
+            },
+            {
+              "featureType": "poi.park",
+              "elementType": "geometry",
+              "stylers": [
+                {
+                  "color": "#e5e5e5"
+                }
+              ]
+            },
+            {
+              "featureType": "poi.park",
+              "elementType": "labels.text.fill",
+              "stylers": [
+                {
+                  "color": "#9e9e9e"
+                }
+              ]
+            },
+            {
+              "featureType": "road",
+              "elementType": "geometry",
+              "stylers": [
+                {
+                  "color": "#ffffff"
+                }
+              ]
+            },
+            {
+              "featureType": "road.arterial",
+              "elementType": "labels.text.fill",
+              "stylers": [
+                {
+                  "color": "#757575"
+                }
+              ]
+            },
+            {
+              "featureType": "road.highway",
+              "elementType": "geometry",
+              "stylers": [
+                {
+                  "color": "#dadada"
+                }
+              ]
+            },
+            {
+              "featureType": "road.highway",
+              "elementType": "labels.text.fill",
+              "stylers": [
+                {
+                  "color": "#616161"
+                }
+              ]
+            },
+            {
+              "featureType": "road.local",
+              "elementType": "labels.text.fill",
+              "stylers": [
+                {
+                  "color": "#9e9e9e"
+                }
+              ]
+            },
+            {
+              "featureType": "transit.line",
+              "elementType": "geometry",
+              "stylers": [
+                {
+                  "color": "#e5e5e5"
+                }
+              ]
+            },
+            {
+              "featureType": "transit.station",
+              "elementType": "geometry",
+              "stylers": [
+                {
+                  "color": "#eeeeee"
+                }
+              ]
+            },
+            {
+              "featureType": "water",
+              "elementType": "geometry",
+              "stylers": [
+                {
+                  "color": "#c9c9c9"
+                }
+              ]
+            },
+            {
+              "featureType": "water",
+              "elementType": "labels.text.fill",
+              "stylers": [
+                {
+                  "color": "#9e9e9e"
+                }
+              ]
+            }
+          ],
+          {name: 'Styled Map'});
+
+    var request = {
+      query: 'Commissoner of Police',
+      fields: ['name', 'geometry'],
+    };
+
+    var mapProp = {
+      center: new google.maps.LatLng(13.0827, 80.2707),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+
+     //Associate the styled map with the MapTypeId and set it to display.
+    this.map.mapTypes.set('styled_map', styledMapType);
+     this.map.setMapTypeId('styled_map');
+    
+     this.service = new google.maps.places.PlacesService(this.map);
+
+    this.service.findPlaceFromQuery(request, function(results, status) {
+          if (status === google.maps.places.PlacesServiceStatus.OK) {
+            for (var i = 0; i < results.length; i++) {
+              this.createMarker(results[i], this.map);
+            }
+
+            this.map.setCenter(results[0].geometry.location);
+          }
+        });
+  }
+  
+  createMarker(place:any, map:any):void  {
+    var marker = new google.maps.Marker({
+      map: map,
+      position: place.geometry.location
+    });
+}
 }
